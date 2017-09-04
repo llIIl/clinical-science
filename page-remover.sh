4bc8c8935f1044413c19c362bbef678f545f193e
1cawk <1cawk@hotmail.com>
2017-09-04 11:05:10 +1000

    added bash scripts that are not complete

---
+++ page-remover.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo -n "Hello $USER, how many pages does the bullshit lecture have in total? "
+read MaxPage
+
+
+echo What pages do you want to remove?
+read page1 page2 page3 page4 page5
+
+
+
+# Multiple numebr of inputs 
+
+# Check they are all numbers else echo Type in some numbers you twat
+
+
+
+# echo "What is your .pdf called? Leave out the .pdf and it is caps sensitive. "
+#read pdfdoc
+
+#pdfseparate -f 1 -l $MaxPage $pdfdoc.pdf output-%d.pdf
+#rm output-$page1.pdf
+#rm $pdfdoc.pdf
+#pdfunite $(ls -v *.pdf) complete.pdf
+#rm output*
+
+echo "page1 $page1"
+echo "page2 $page2"
+echo "page3 $page5"
+
+echo Should be right
+exit
